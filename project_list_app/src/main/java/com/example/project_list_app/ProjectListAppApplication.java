1855ea2e3b6a5d1c1ca5d9776f2dab15feb1ca33
Pethum-Jayakodi <jayakopethum6@gmail.com>
2024-04-08 10:46:22 +0530

    spring initializer

---
+++ project_list_app/src/main/java/com/example/project_list_app/ProjectListAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_list_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectListAppApplication.class, args);
+	}
+
+}
---
+++ project_list_app/src/test/java/com/example/project_list_app/ProjectListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_list_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
